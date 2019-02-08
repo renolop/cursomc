9564c5c8b1d65930b5b13fc16314a677837349bd
Renato Lopes <renolop@yahoo.com.br>
2019-02-08 18:02:47 -0200

    Paginação

--- src/main/java/com/renolop/cursomc/CursomcApplication.java
+++ src/main/java/com/renolop/cursomc/CursomcApplication.java
@@ -69,6 +69,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório" );
+		Categoria cat3 = new Categoria(null, "Cama Mesa Banho" );
+		Categoria cat4 = new Categoria(null, "Eletrônicos" );
+		Categoria cat5 = new Categoria(null, "Jardinagem" );
+		Categoria cat6 = new Categoria(null, "Decoração" );
+		Categoria cat7 = new Categoria(null, "Casa e Cozinha" );
 
 		Produto p1 = new Produto(null, 2000.00, "Computador");
 		Produto p2 = new Produto(null, 800.00, "Impressora");
@@ -81,7 +86,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll( Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll( Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll( Arrays.asList(p1, p2, p3));
 		
 		Estado estado1 = new Estado(null, "Minas Gerais");
